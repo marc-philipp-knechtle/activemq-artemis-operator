57ec6b54acca39fea8fb02d7f9a97296ae9d058f
Marc-Philipp Knechtle <marc-philipp.knechtle@anevis-solutions.com>
2024-06-19 14:49:01 +0200

    add ingress configuration for the webconsole

---
+++ anevis/patch_all_ingresses.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NAMESPACE="activemq-artemis-operator"
+
+PATCH_FILE="ing-patch.yml"
+
+INGRESSES=$(kubectl get ingress -n $NAMESPACE -o jsonpath='{.items[*].metadata.name}')
+
+# Loop through each Ingress and apply the patch
+for INGRESS in $INGRESSES; do
+  echo "Patching ingress: $INGRESS"
+  kubectl patch ingress $INGRESS -n $NAMESPACE --patch "$(cat $PATCH_FILE)"
+done
+
